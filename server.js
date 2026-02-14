41d22a57cd59a757e270d2592ffdedab71481d40
Aman Jaiswal <amanajsjrj@gmail.com>
2026-02-14 13:01:11 +0530

    cors issue resolved

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "https://connecto-frontend.onrender.com"
   ],
   credentials: true

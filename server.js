2dfc14ecde9c5f7b505f1fa1c84141d0e70c2395
Aman Jaiswal <amanajsjrj@gmail.com>
2026-02-14 11:01:18 +0530

    b commit

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+//app.use(cors());
+//https://connecto-frontend.onrender.com
+app.use(cors({ origin: "https://connecto-frontend.onrender.com" }))
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
